732b70e28f021a83010a1b53e43978e3cad1dd56
Dave Lee <camperdave614@gmail.com>
2011-05-08 16:43:55 -0500

    This is the start of another ZSH plugin I'm making for mostly-personal use. It's a function library of stuff not usefull enough to be worth corrupting my mainline oh-my-zsh tree for, but that I still would like to have. Clone if you want, but it's pretty sparse right now...

---
+++ misc-util.plugin.zsh
@@ -0,0 +1,7 @@
+# Various Utility functions that are probably not useful enough to get mainline'd, but I like to keep around anyway :P
+# Created by Dave Lee on 5/8/2011
+
+noext() {
+    echo ${1/.*/}
+}
+
